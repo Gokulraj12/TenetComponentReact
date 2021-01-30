4ab0bc56c19e2265086182d9773dd718bed38c77
Gokulraj12 <gokul799@gmail.com>
2021-01-30 15:00:10 +0530

    comment structure created

--- tenetcomponents/src/index.js
+++ tenetcomponents/src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 const App = () => {
     return (
-        <div className="ui-container-comments">
-            <div className="comments">
+        <div className="ui container comments">
+            <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avator-icon" />
+                    <img alt="avator-icon" src="https://source.unsplash.com/random" style={{ height: '50px', width: '50px', paddingRight: "10px" }} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
                         Athira
-                    </a>
-                </div>
-                <div className="metadata">
-                    <span className="date">Today at time</span>
-                </div>
-                <div className="text">
-                    First Comment!
+                        </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">First Comment!</div>
                 </div>
             </div>
         </div>
