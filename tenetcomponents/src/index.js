73cb08be851247fe997d42739b63c7d8892cb301
Gokulraj12 <gokul799@gmail.com>
2021-01-30 17:03:30 +0530

    ApprovalCard implementation

--- tenetcomponents/src/index.js
+++ tenetcomponents/src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './commentdetail';
+import ApprovalCard from './ApprovalCard';
+import CommentDetail from './CommentDetail';
+
 
 
 const App = () => {
     return (
         <div className="ui container comments">
             {/*Component Nesting*/}
-            <CommentDetail
-                name="Gokul"
-                text="First Comment!"
-                avatar="https://source.unsplash.com/random/"
-            />
-            <CommentDetail
-                name="Ram"
-                text="Second comment!"
-                avatar="https://source.unsplash.com/random"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    name="Gokul"
+                    text="First Comment!"
+                    avatar="https://source.unsplash.com/random/"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    name="Ram"
+                    text="Second comment!"
+                    avatar="https://source.unsplash.com/random"
+                />
+            </ApprovalCard>
         </div>
     );
 };
