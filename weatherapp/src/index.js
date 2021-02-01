782b5b60ff48c7bbfee0b5a68f6d4b63f1a71e90
Gokulraj12 <gokul799@gmail.com>
2021-02-01 11:36:44 +0530

    refactored App Component to class Component

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WeatherDisplay from './WeatherDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => { console.log(position); },
-    err => { console.log(err); }
-  );
-  return (
-    <div>
-      <WeatherDisplay />
-    </div>
-  );
-};
+//import WeatherDisplay from './WeatherDisplay';
+
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => { console.log(position); },
+//     err => { console.log(err); }
+//   );
+//   return (
+//     <div>
+//       Latitude:
+//       <WeatherDisplay />
+//     </div>
+//   );
+// };
+
+//Class component refactoring
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //initiating state JS object
+    this.state = { lat: null }
+  }
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => { console.log(position); },
+      err => { console.log(err); }
+    );
+    return <div>Latitude: </div>
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
