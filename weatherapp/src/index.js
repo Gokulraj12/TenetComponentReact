0f713e0a8dcc506f9c2fa94f25abacbb99af7752
Gokulraj12 <gokul799@gmail.com>
2021-02-01 22:02:40 +0530

    Setting error message

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -21,18 +21,26 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     //initiating state JS object and this is the only time we do direct assignment to this state ,in all other cases we use setState
-    this.state = { lat: null }
+    this.state = { lat: null, errorMessage: "" }
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => { console.log(err); }
+      err => {
+        //console.log(err);
+        this.setState({ errorMessage: err.message })
+      }
     );
 
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>
+    return (
+      <div>Latitude: {this.state.lat}
+        <br />
+      Error:{this.state.errorMessage}
+      </div>
+    );
   }
 }
 
