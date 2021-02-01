b8b55e8294bf583a3f7a3cadd5f5c50f0f467f45
Gokulraj12 <gokul799@gmail.com>
2021-02-01 11:52:54 +0530

    setState() call implementation

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -20,15 +20,19 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    //initiating state JS object
+    //initiating state JS object and this is the only time we do direct assignment to this state ,in all other cases we use setState
     this.state = { lat: null }
-  }
-  render() {
+
     window.navigator.geolocation.getCurrentPosition(
-      position => { console.log(position); },
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => { console.log(err); }
     );
-    return <div>Latitude: </div>
+
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
