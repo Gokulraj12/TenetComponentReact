9cd64ce36eb64ac0f83ce4895ca3fa555c2da8cf
Gokulraj12 <gokul799@gmail.com>
2021-02-10 23:11:13 +0530

    Gave styling to Weatherdisplay

--- weatherapp/src/WeatherDisplay.js
+++ weatherapp/src/WeatherDisplay.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './WeatherDisplay.css';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr its chilly",
+        iconName: "snowflake"
+    }
+};
 
 const getSeason = (lat, months) => {
     if (months > 2 && months < 9) {
@@ -13,10 +25,15 @@ const WeatherDisplay = props => {
 
     //const date = new Date().getMonths();
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
+    // const userDefinedText = season === 'winter' ? "Burr its chilly!" : "Let's hit the beach!";
+    const { text, iconName } = seasonConfig[season];
 
     return (
-        <div>WeatherDisplay</div>
+        <div className={`weather-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
     );
 };
 
