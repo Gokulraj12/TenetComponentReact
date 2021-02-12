120e537de2f2610b0b8afe5ec7d03af75940bd31
Gokulraj12 <gokul799@gmail.com>
2021-02-12 11:43:28 +0530

    onSubmitForm function implementation

--- feedbacksearch/src/components/SearchBar.js
+++ feedbacksearch/src/components/SearchBar.js
@@ -6,13 +6,23 @@ class SearchBar extends React.Component {
     //     console.log(event.target.value);
     // }
 
+    //Controlled Component
+    state = { searchkey: "" }
+
+    //OnSubmitFunction -preventing default behavior of browser with preventDefault function
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(this.state.searchkey)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={event => { console.log(event.target.value) }} />
+                        <input type="text" value={this.state.searchkey} onChange={event => { this.setState({ searchkey: event.target.value }); console.log(event.target.value) }} />
                     </div>
                     {/* <button type='search'>Search</button> */}
                 </form>
