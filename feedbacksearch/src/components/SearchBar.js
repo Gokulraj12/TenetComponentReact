83b48496b437414f4264ed8f5e1d77d24dd0134b
Gokulraj12 <gokul799@gmail.com>
2021-02-12 09:01:16 +0530

    onchange event added for user search

--- feedbacksearch/src/components/SearchBar.js
+++ feedbacksearch/src/components/SearchBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
+
     render() {
         return (
-            <div>Searchbar</div>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" onChange={event => { console.log(event.target.value) }} />
+                    </div>
+                    {/* <button type='search'>Search</button> */}
+                </form>
+            </div>
         );
     }
 }
