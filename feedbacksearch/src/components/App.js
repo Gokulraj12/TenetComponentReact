1ea9816b9d4437f8649cc7b521922000799f15e6
Gokulraj12 <gokul799@gmail.com>
2021-02-16 10:01:51 +0530

    async await/then

--- feedbacksearch/src/components/App.js
+++ feedbacksearch/src/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit = (searchkey) => {
-        console.log(searchkey);
+    // onSearchSubmit = (searchkey) => {
+    //     //console.log(searchkey);
+    //     axios.get("https://api.unsplash.com/search/photos", {
+    //         params: { query: searchkey },
+    //         headers: {
+    //             Authorization: 'Client-ID R2Wd436bsXK0eHIfmB4VqhEhgkNmHn5aDS1AtgPrYrs'
+    //         }
+    //         //when we have to wait for the api response ,we can use then /async functions
+    //     }).then(response => {
+    //         console.log(response.data.results);
+    //     });
+    // };
+
+
+    //Another way fo waiting for the api to return for the response
+    async onSearchSubmit(searchkey) {
+        //console.log(searchkey);
+        const response = await axios.get("https://api.unsplash.com/search/photos", {
+            params: { query: searchkey },
+            headers: {
+                Authorization: 'Client-ID R2Wd436bsXK0eHIfmB4VqhEhgkNmHn5aDS1AtgPrYrs'
+            }
+            //when we have to wait for the api response ,we can use then /async functions
+        });
+        console.log(response.data.results);
     };
 
     render() {
