93fc263c84a0abc1c05cd8780fccc5633024ae4b
Gokulraj12 <gokul799@gmail.com>
2021-02-16 15:12:41 +0530

    Imagecard creation

--- feedbacksearch/src/components/ImageList.js
+++ feedbacksearch/src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
     //desttructing props
-    const images = props.images.map(({ urls, description, id }) => {
-        return <img src={urls.regular} alt={description} key={id} />
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
