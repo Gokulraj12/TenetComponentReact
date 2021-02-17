a57de31ab3de9d5dc55410c7f442f568ccd7884f
Gokulraj12 <gokul799@gmail.com>
2021-02-17 10:33:07 +0530

    final commit v2 implementation

--- feedbacksearch/src/components/ImageCard.js
+++ feedbacksearch/src/components/ImageCard.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props)
+        //creating a  reference variable to
+
+        this.state = { spans: 0 };
+
+        this.imageRef = React.createRef();
+
+
+    }
+
+    componentDidMount() {
+        //console.log(this.imageRef);
+        this.imageRef.current.addEventListener("load", this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+        console.log(spans);
+
+        this.setState({ spans });
+    }
+
+
     render() {
         const { description, urls } = this.props.image; //destructuring
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img alt={description}
                     src={urls.regular}
+                    ref={this.imageRef}
                 />
             </div>
         );
