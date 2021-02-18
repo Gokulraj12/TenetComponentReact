c0711c2d2ca5a4e539e7906d0199cea6727de1a8
Gokulraj12 <gokul799@gmail.com>
2021-02-18 08:16:04 +0530

    form creation

---
+++ youtubeapp/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+    state = { searchkey: "" };
+
+    onSearchParamChange = event => {
+        this.setState({ searchkey: event.target.value });
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+    };
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className='ui form' onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" placeholder="search videos..." onChange={this.onSearchParamChange} value={this.state.searchkey} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
